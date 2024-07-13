dba744c80937e00e542eda18b85454231257cdde
Ayla-T <116853495+Ayla-T@users.noreply.github.com>
2024-07-13 14:51:40 -0400

    Python update

--- mus_rec_sys.py
+++ mus_rec_sys.py
@@ -1,7 +1,29 @@
 import pandas as pd
+
 df = pd.read_csv("spotify_millsongdata.csv")
+
+#Data Cleaning
 df = df.drop('link', axis = 1).reset_index(drop = True)
+
+#Sampling
 df = df.sample(5000)
-df['text'][0]
+
 #Text Cleaning/Text Preprocessing
 df['text'] = df['text'].str.lower().replace(r'^\w\s',' ').replace(r'\n', ' ', regex= True)
+
+#Tokenization
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+stemmer = PorterStemmer()
+
+def = token(txt):
+    token = nltk.word_tokenize(txt)
+    a = [stemmer.stem(w) for w in token]
+    return " ".join(a)
+
+#from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.metrics.pairwise import cosine_similarity
+
+#tfid = TfidfVectorizer(analyzer='word', stop_words='english')
+#matrix = tfid.fit_transform(df['text'])
