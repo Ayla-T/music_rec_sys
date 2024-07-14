74c02c6fdd50994268597c5f85ae9e1c593e8e6a
Ayla-T <116853495+Ayla-T@users.noreply.github.com>
2024-07-14 13:08:31 -0400

    mus_rec_sys.py completo

--- mus_rec_sys.py
+++ mus_rec_sys.py
@@ -15,6 +15,9 @@ df['text'] = df['text'].str.lower().replace(r'^\w\s',' ').replace(r'\n', ' ', re
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+# Download the 'punkt' resource
+nltk.download('punkt')  # Download the necessary resource for tokenization
+
 stemmer = PorterStemmer()
 
 def token(txt):
@@ -22,9 +25,27 @@ def token(txt):
     a = [stemmer.stem(w) for w in token]
     return " ".join(a)
 
+df['text'] = df['text'].apply(lambda x: token(x))
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 tfid = TfidfVectorizer(analyzer='word', stop_words='english')
 matrix = tfid.fit_transform(df['text'])
+
+#Cosine Similarity
+similar = cosine_similarity(matrix)
+
+#Recommendation Function
+def recommendation(song_df):
+    idx = df[df['song'] == song_df].index[0]
+    distance = sorted(list(enumerate(similar[idx])), reverse=True, key=lambda x: x[1])
+    songs = []
+    for s_id in distance[1:5]:
+        songs.append(df.iloc[s_id[0]].song)
+    return songs
+
+import pickle
+pickle.dump(similar, open('similarity.pkl', 'wb'))
+pickle.dump(df, open('df.pkl', 'wb'))
 
